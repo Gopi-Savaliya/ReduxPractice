b3cfbe28c5a87b4ae96d6eae8dd4789bee5996b1
Gopi Savaliya <gopis@Gopis-MacBook-Air.local>
2022-02-15 12:41:56 +0530

    Redux implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import store from './components/Store';
+import { bugAdded, bugRemoved, bugResolved } from './components/Actions'
 
 function App() {
+
+  // const unsubscribe = store.subscribe(() => {
+  //   console.log("Store changed!", store.getState())
+  // });
+
+  store.subscribe(() => {
+      console.log("Store changed!", store.getState())
+    });
+
+  store.dispatch(bugAdded("bug 1"));
+  // unsubscribe();
+  store.dispatch(bugResolved(1));
+  store.dispatch(bugRemoved(1));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Redux</h1>
+      <p>Watch console for result</p>
     </div>
   );
 }
